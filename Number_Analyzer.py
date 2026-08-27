6e264ebd431e4df4e2b19c8a7c37a2e213b23462
Syed Momin <syedmomin2026@gmail.com>
2026-08-27 22:41:50 +0530

    Number_Analyzer

--- Number_Analyzer.py
+++ Number_Analyzer.py
@@ -3,10 +3,10 @@ print("This is a program to analyze the number")
 n = int(input("Enter the number:"))
 
 print("What do you want to analyze?")
-print("even or odd / positive, negative or zero / prime or composite number")
+print("even or odd / positive, negative or zero / prime or composite number / no. of digits")
 
 k = input("Enter your option: ")
-l = ["even or odd", "positive, negative or zero", "prime or composite number"]
+l = ["even or odd", "positive, negative or zero", "prime or composite number", "no. of digits"]
 
 if k not in l:
     print("You chose the wrong option, try by running the code again")
@@ -30,10 +30,23 @@ else:
             c = "negative"
             print(c)
     elif k == "prime or composite number":
-            s = 
+            s = 0
             for i in range(1, n + 1):
-                if n%i == 1:
-                    print("composite number")
-                    break
-                elif n%i == 0:
-                    print("prime number")
+                if n%i == 0:
+                    s += 1
+            if n == 0:
+                print("It is neither prime nor composite number")
+            elif n < 0:
+                print("Invalid number")
+            elif s == 2:
+                print("prime number")
+            else:
+                print("composite number")
+
+    elif k == "no. of digits":
+        s = str(n)
+        if n < 0:
+            f = len(s) - 1
+            print(f)
+        else:
+            print(len(s))
